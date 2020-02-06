021fa9bf1181a9e87cb28e74ee03f5bdc961ae38
Logan Ellis <mrloganellis@gmail.com>
2020-02-06 10:58:45 -0500

    GooglePlayApps server made

---
+++ app.js
@@ -0,0 +1,52 @@
+const express = require('express');
+
+const morgan =  require('morgan')
+
+const app = express();
+
+app.use(morgan('common'))
+
+const playStoreApps = require('./playstoreapps/playstoreapps.js')
+
+app.get('/apps', (req, res) => {
+
+    const {sort, genres} = req.query;
+
+    if (sort) {
+        if(![rating, app].include(sort)) {
+            return res
+                    .status(400)
+                    .send('Sort must by ratings or apps')
+        }
+    }    
+
+    if (genres) {
+        if(![Action, Puzzle, Strategy, Casual, Arcade, Card].include(genres)) {
+            return res 
+                    .status(400)
+                    .send('Genres must be action, puzzle, strategy, casual, arcade, or card');
+        }
+    }
+
+    let results = playStoreApps
+        .filter(playStoreApps => 
+            playStoreApps
+                .app
+                .toLowerCase()
+                .includes(genres.toLowerCase()));
+
+    if (sort) {
+        results.sort((a, b) => {
+            return a[sort] > b[sort] ? 1 : a[sort] < b[sort] ? -1 : 0;
+        })
+    }
+
+
+    res
+        .json(results)
+
+})
+
+app.listen(8000, () => {
+    console.log('Server is listening at PORT 8000')
+})
